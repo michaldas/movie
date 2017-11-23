30c7e54005b95710f4f0ded16b2841192298f677
Michal Dasberg <michaldasb@gmail.com>
2017-11-23 13:43:07 +0200

    routing

--- movieApp.js
+++ movieApp.js
@@ -1,1 +1,17 @@
-var app = angular.module("movieApp", []);
+var app = angular.module("movieApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl: "actors.html",
+      controller: "actorCtrl"
+    })
+    .when("/movies", {
+      templateUrl: "movies.html",
+      controller: "moviesCtrl"
+    })
+    .otherwise({
+      redirectTo: "/"
+    });
+    
+  });
